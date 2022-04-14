9e7946e5ea382a19774c0135ab4ed2b182b9fbcb
Julian Pollinger <julian.pollinger@jp-studios.de>
2022-04-14 18:20:38 +0200

    github action

---
+++ .releaserc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    branches: ['main', { name: 'next', channel: 'next', prerelease: true }],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'docs/CHANGELOG.md',
+            },
+        ],
+        '@semantic-release/npm',
+        [
+            '@semantic-release/github',
+            {
+                assets: ['dist_zip/dist.zip', 'dist_zip/schema.zip'],
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json', 'docs/*', 'docs/**/*'],
+                message:
+                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+    preset: 'angular',
+};
